fe91bc5f1a83bc089c87374fbbb0006c41523678
rznd <grezende310@gmail.com>
2024-05-06 22:26:25 -0300

    atualizado

--- app/exibirApenasDisponiveis.js
+++ app/exibirApenasDisponiveis.js
@@ -1,7 +1,22 @@
 const bntLivrosDisponiveis = document.getElementById('btnLivrosDisponiveis')
 bntLivrosDisponiveis.addEventListener('click', exibirApenasDisponiveis)
 
+const elementoValorTotalLivros2 = document.getElementById('valor_total_livros_disponiveis')
+
 function exibirApenasDisponiveis() {
-    let livrosDisponiveis = livros.filter(livro => livro.quantidade > 0)
+    let livrosDisponiveis = livrosComDesconto.filter(livro => livro.quantidade > 0)
+   
     exibirLivros(livrosDisponiveis)
+    exibirValorTotalLivrosDisponiveis(livrosDisponiveis)
+
 }
+
+function exibirValorTotalLivrosDisponiveis(livrosDisponiveis) {
+    let valorTotal = livrosDisponiveis.map(livro => livro.preco).reduce((a, b) => a + b)
+    
+    elementoValorTotalLivros2.innerHTML = `   
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal.toFixed(2)}</span></p>
+        </div>
+    `
+} 
--- app/exibirLivros.js
+++ app/exibirLivros.js
@@ -1,25 +1,29 @@
 const elementoInserirLivros = document.getElementById('livros')
+const elementoValorTotalLivros = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirLivros(listaLivros) {
+    console.log('exibir')
     elementoInserirLivros.innerHTML = ''
+    elementoValorTotalLivros.innerHTML = ''
+
     
-
     listaLivros.forEach(livro => {
+        
         let disponibilidade = (livro.quantidade <= 0) ? 'indisponivel' : ''
-
+        
         elementoInserirLivros.innerHTML += `
-            <div class="livro">
-                <img class="livro__imagens ${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
-                <h2 class="livro__titulo">
-                    ${livro.titulo}
-                </h2>
-                <p class="livro__descricao">${livro.autor}</p>
-                <p class="livro__preco" id="preco">R$ ${parseFloat(livro.preco.toFixed(2))}</p>
-                <div class="tags">
-                    <span class="tag">${livro.categoria}</span>
-                </div>
-         </div>
+        <div class="livro">
+        <img class="livro__imagens ${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+        ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${parseFloat(livro.preco.toFixed(2))}</p>
+        <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
         `
     })
-
+    
 }
